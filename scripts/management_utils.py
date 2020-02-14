082d06ef6f6b99c8a425e0987f09012fda15ceea
grewwc129 <grewwc@gmail.com>
2020-02-14 10:45:20 +0800

    bug fixes

--- scripts/management_utils.py
+++ scripts/management_utils.py
@@ -5,6 +5,6 @@ import warnings
 def Deperacated(func):
     @wraps(func)
     def inner(*args, **kwargs):
-        func(*args, **kwargs)
         warnings.warn("Deperacated", UserWarning)
+        return func(*args, **kwargs)
     return inner
